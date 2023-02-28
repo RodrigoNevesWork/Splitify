2092ea46072444462c9d7e57cc61531d833f7bc5
Rodrigo Neves <94999746+RodrigoNevesWork@users.noreply.github.com>
2023-02-28 22:01:01 +0000

    Start of a Spring API

---
+++ Splitify/Splitify/src/main/kotlin/Project/Splitify/SplitifyApplication.kt
@@ -0,0 +1,11 @@
+package Project.Splitify
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SplitifyApplication
+
+fun main(args: Array<String>) {
+	runApplication<SplitifyApplication>(*args)
+}
---
+++ Splitify/Splitify/src/test/kotlin/Project/Splitify/SplitifyApplicationTests.kt
@@ -0,0 +1,13 @@
+package Project.Splitify
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SplitifyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
